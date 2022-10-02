84c051e570ed4211e54a1bf0f53b16a8d0edaad2
FloSojer <flo.sojer@gmail.com>
2022-10-02 20:03:55 +0200

    different changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getPositionOfLineAndCharacter } from 'typescript';
 
 function App() {
+
+  const [loc, setLoc] = useState(null);
+  console.log(window.location.href);
+
+  //setLoc()
+
+  function getPos() : any {
+    let currentPositionx = window.screenTop;
+    let currentPositionY = window.screenLeft;
+    let loc = window.location.href; 
+  
+    console.log('POS TOP', currentPositionx);
+    console.log('POS LEFT', currentPositionY);
+    console.log('loc', loc);
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <div className='outerhomepage'> 
+     
+          <div className='homepage'>
+            <div className='page home'>
+              hallo
+              <button onClick={getPos}> POS </button>
+            </div>
+            <div className=' page programming'>
+              Prog
+              <button onClick={getPos}> POS </button>
+            </div>
+            <div className='page photography'>
+                Foto
+                <button onClick={getPos}> POS </button>
+            </div>
+          </div>
+      </div>
+      <div className='topBar'> Top Bar </div>
+    </>
   );
 }
 
